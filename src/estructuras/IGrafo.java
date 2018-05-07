a1610f494dc7c772ab267cbe301451d631478c31
danielgalvis98 <dangaltor@gmail.com>
2018-05-07 15:27:03 -0500

    quitado

--- src/estructuras/IGrafo.java
+++ src/estructuras/IGrafo.java
@@ -3,12 +3,11 @@ package estructuras;
 public interface IGrafo<T> {
 
 	public void agregarNodo (T nodo) throws Exception;
-	public void generarArista (T nodo1, T nodo2, double peso);
+	public void generarArista (T nodo1, T nodo2, double peso) throws Exception;
 	public int recorridoBFS (T nodoInicial) throws Exception;
 	public int recorridoBFS ();
 	public void recorridoDFS();
 	public double [] [] prim();
-	public Nodo darNodo (T nodo) throws Exception;
 	public double [] [] Kruskal();
 	public Nodo Dijkstra  (T nodo1, T nodo2) throws Exception;
 	public Nodo FloydWarshall (T nodo1, T nodo2) throws Exception;
