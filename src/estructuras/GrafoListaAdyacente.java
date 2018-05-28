15e92610bf58fa5d749829a8832d5d6b9dcaca62
david1999erazo <33490042+david1999erazo@users.noreply.github.com>
2018-05-27 21:21:43 -0500

    kruskal111

--- src/estructuras/GrafoListaAdyacente.java
+++ src/estructuras/GrafoListaAdyacente.java
@@ -1,8 +1,10 @@
 package estructuras;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Set;
@@ -10,72 +12,72 @@ import java.util.Set;
 public class GrafoListaAdyacente<E> implements IGrafo<E> {
 
 	private HashMap<E, NodoListaAdyacente<E>> nodos;
-	
+
 	int maxNodos;
 	int totalNodos;
-	
-	public GrafoListaAdyacente( int maxNodos) {
-		nodos= new HashMap<>();
+
+	public GrafoListaAdyacente(int maxNodos) {
+		nodos = new HashMap<>();
 		this.maxNodos = maxNodos;
 		totalNodos = 0;
 	}
 
 	@Override
 	public void agregarNodo(E nodo) throws Exception {
-		NodoListaAdyacente<E> nuevo= new NodoListaAdyacente<E>(nodo);
+		NodoListaAdyacente<E> nuevo = new NodoListaAdyacente<E>(nodo);
 		nodos.put(nodo, nuevo);
-		
+
 	}
 
 	@Override
 	public void generarArista(E nodo1, E nodo2, double peso) throws Exception {
 		NodoListaAdyacente<E> n1 = nodos.get(nodo1);
-		NodoListaAdyacente<E> n2= nodos.get(nodo2);
-		
+		NodoListaAdyacente<E> n2 = nodos.get(nodo2);
+
 		n1.agregarAdyacente(n2);
 		n2.agregarAdyacente(n1);
-		
+
 		n1.agregarPeso(n2, peso);
 		n2.agregarPeso(n1, peso);
 	}
 
 	@Override
 	public int recorridoBFS(E nodoInicial) throws Exception {
-		for(E na: nodos.keySet()){
+		for (E na : nodos.keySet()) {
 			nodos.get(na).setRevisado(false);
 		}
-		int encontrados=0;
-		NodoListaAdyacente<E> actual= nodos.get(nodoInicial);
-		if(actual == null)
+		int encontrados = 0;
+		NodoListaAdyacente<E> actual = nodos.get(nodoInicial);
+		if (actual == null)
 			throw new Exception("Nodo existe el nodo");
-		Queue<NodoListaAdyacente<E>> cola= new LinkedList<>();
+		Queue<NodoListaAdyacente<E>> cola = new LinkedList<>();
 		cola.add(actual);
 		actual.setRevisado(true);
-		while(!cola.isEmpty()){
-			NodoListaAdyacente<E> revisando= cola.poll();
-			ArrayList<INodoLista<E>> adyacentes= revisando.darAdyacentes();
-			for(int i=0;i<adyacentes.size();i++){
-				if(!adyacentes.get(i).isRevisado()){
+		while (!cola.isEmpty()) {
+			NodoListaAdyacente<E> revisando = cola.poll();
+			ArrayList<INodoLista<E>> adyacentes = revisando.darAdyacentes();
+			for (int i = 0; i < adyacentes.size(); i++) {
+				if (!adyacentes.get(i).isRevisado()) {
 					adyacentes.get(i).setRevisado(true);
 					encontrados++;
 					adyacentes.get(i).setPadre(revisando);
-					cola.add((NodoListaAdyacente<E>)adyacentes.get(i));
+					cola.add((NodoListaAdyacente<E>) adyacentes.get(i));
 				}
 			}
 			revisando.setRevisado(true);
 		}
 		return encontrados;
-		
+
 	}
 
 	@Override
 	public int recorridoBFS() {
 		int respuesta;
-		Set<E> llaves =  nodos.keySet();
+		Set<E> llaves = nodos.keySet();
 		try {
 			respuesta = recorridoBFS(llaves.iterator().next());
 		} catch (Exception e) {
-			respuesta=0;
+			respuesta = 0;
 		}
 		return respuesta;
 	}
@@ -83,134 +85,153 @@ public class GrafoListaAdyacente<E> implements IGrafo<E> {
 	@Override
 	public void recorridoDFS() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public IGrafo<E> prim() throws Exception {
-		int cantEsperada= nodos.size();
-		int cantReal=recorridoBFS();
-		if(cantEsperada!= cantReal)
+		int cantEsperada = nodos.size();
+		int cantReal = recorridoBFS();
+		if (cantEsperada != cantReal)
 			throw new Exception("Grafo no conexo");
 		PriorityQueue<NodoPesoAdyacente<NodoListaAdyacente<E>>> cola = new PriorityQueue<>();
-		GrafoListaAdyacente<E> retorno= new GrafoListaAdyacente<>(maxNodos); //agrege maxNodos
-		HashMap<E, NodoListaAdyacente<E>> nodosNuevos= new HashMap<>();
-		for(E na:nodos.keySet()){
+		GrafoListaAdyacente<E> retorno = new GrafoListaAdyacente<>(maxNodos); // agrege maxNodos
+		HashMap<E, NodoListaAdyacente<E>> nodosNuevos = new HashMap<>();
+		for (E na : nodos.keySet()) {
 			NodoListaAdyacente<E> actual = nodos.get(na);
-			E elemActual=actual.getElemento();
+			E elemActual = actual.getElemento();
 			actual.setRevisado(false);
 			retorno.agregarNodo(elemActual);
-			NodoListaAdyacente<E> nuevo= retorno.darNodo(elemActual);
+			NodoListaAdyacente<E> nuevo = retorno.darNodo(elemActual);
 			nodosNuevos.put(elemActual, nuevo);
-			ArrayList<NodoListaAdyacente<E>> adyacentes= new ArrayList<>();
-			for(int i=0;i<adyacentes.size();i++){
-				cola.add(new NodoPesoAdyacente<NodoListaAdyacente<E>>(actual, actual.darPesoAdyacente(adyacentes.get(i)),adyacentes.get(i) ));
+			ArrayList<NodoListaAdyacente<E>> adyacentes = new ArrayList<>();
+			for (int i = 0; i < adyacentes.size(); i++) {
+				cola.add(new NodoPesoAdyacente<NodoListaAdyacente<E>>(actual,
+						actual.darPesoAdyacente(adyacentes.get(i)), adyacentes.get(i)));
 			}
-			
+
 		}
-		NodoPesoAdyacente<NodoListaAdyacente<E>> aristaMenor= cola.poll();
-		NodoListaAdyacente<E> actual= aristaMenor.getNodo();
+		NodoPesoAdyacente<NodoListaAdyacente<E>> aristaMenor = cola.poll();
+		NodoListaAdyacente<E> actual = aristaMenor.getNodo();
 		actual.setRevisado(true);
 		retorno.generarArista(actual.getElemento(), aristaMenor.getAdyacente().getElemento(), aristaMenor.getPeso());
-		cola=new PriorityQueue<>();
-		actual=aristaMenor.getAdyacente();
-		
-		while(!cola.isEmpty()){
+		cola = new PriorityQueue<>();
+		actual = aristaMenor.getAdyacente();
+
+		while (!cola.isEmpty()) {
 			actual.setRevisado(true);
-			ArrayList<NodoListaAdyacente<E>> adyacentes= new ArrayList<>();
-			for(int i=0;i<adyacentes.size();i++){
-				NodoListaAdyacente<E> adyacenteActual= adyacentes.get(i);
-				if(!adyacenteActual.isRevisado()){
-					NodoPesoAdyacente<NodoListaAdyacente<E>> aristaNueva=new NodoPesoAdyacente<NodoListaAdyacente<E>>(actual, actual.darPesoAdyacente(adyacenteActual), adyacenteActual);
+			ArrayList<NodoListaAdyacente<E>> adyacentes = new ArrayList<>();
+			for (int i = 0; i < adyacentes.size(); i++) {
+				NodoListaAdyacente<E> adyacenteActual = adyacentes.get(i);
+				if (!adyacenteActual.isRevisado()) {
+					NodoPesoAdyacente<NodoListaAdyacente<E>> aristaNueva = new NodoPesoAdyacente<NodoListaAdyacente<E>>(
+							actual, actual.darPesoAdyacente(adyacenteActual), adyacenteActual);
 					cola.add(aristaNueva);
 				}
 			}
-			boolean agregada=false;
-			while(!agregada){
-				NodoPesoAdyacente<NodoListaAdyacente<E>> aristaSiguiente= cola.poll();
-				if(!aristaSiguiente.getAdyacente().isRevisado()){
-					retorno.generarArista(aristaSiguiente.getNodo().getElemento(), aristaSiguiente.getAdyacente().getElemento(), aristaSiguiente.getPeso());
-					agregada=true;
-					actual=aristaSiguiente.getAdyacente();
+			boolean agregada = false;
+			while (!agregada) {
+				NodoPesoAdyacente<NodoListaAdyacente<E>> aristaSiguiente = cola.poll();
+				if (!aristaSiguiente.getAdyacente().isRevisado()) {
+					retorno.generarArista(aristaSiguiente.getNodo().getElemento(),
+							aristaSiguiente.getAdyacente().getElemento(), aristaSiguiente.getPeso());
+					agregada = true;
+					actual = aristaSiguiente.getAdyacente();
 				}
 			}
-			
-			
+
 		}
-		
 
 		return retorno;
 	}
 
 	@Override
 	public GrafoListaAdyacente<E> Kruskal() {
-		
+
 		GrafoListaAdyacente<E> grafoSalida = new GrafoListaAdyacente<>(maxNodos);
 		ConjuntosDisjuntos conjunto = new ConjuntosDisjuntos(maxNodos);
-		
-		for (int i=0; i< totalNodos ; i++) {
+
+		for (int i = 0; i < totalNodos; i++) {
 			try {
-				grafoSalida.agregarNodo((E)nodos.get(i).getElemento());
+				grafoSalida.agregarNodo((E) nodos.get(i).getElemento());
 				nodos.get(i).setRevisado(false);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			
-			ArrayList<NodoPesoAdyacente<NodoListaAdyacente<E>>> aristas = new ArrayList<>();
-			
-			//Recorrer la lista de adyacencia
-			
+
+		}
+		// Recorrer la lista de adyacencia
+		ArrayList<NodoPesoAdyacente<NodoListaAdyacente<E>>> aristas = new ArrayList<>();
+		
+		
+		
+		for (E key : nodos.keySet()) {
+			NodoListaAdyacente<E> actual = new NodoListaAdyacente<>(key);
+			//NodoPesoAdyacente<NodoListaAdyacente<E>> arista = new NodoPesoAdyacente<NodoListaAdyacente<E>>(actual,actual.darAdyacentes() ,	  );
+			//aristas.add(arista);
 			
 		}
 		
 		
+		Collections.sort(aristas);
 		
+		for(int k=0; k<aristas.size();k++) {
+			NodoPesoAdyacente<NodoListaAdyacente<E>> arista = aristas.get(k); 
+			NodoListaAdyacente<E> i = arista.getNodo();
+			NodoListaAdyacente<E> j = arista.getAdyacente();
+			
+			//if(!conjunto.esElMismo(nodos.get(i).darPesoAdyacente(i), nodos.get(j))) {
+			//	conjunto.union(i, j);
+			//	grafoSalida.generarArista(i.getElemento(), j.getElemento(), arista.getPeso());
+			//}
+		}
+
 		return grafoSalida;
 	}
 
 	@Override
 	public ListaPeso Dijkstra(E nodo1, E nodo2) throws Exception {
-		HashMap<NodoListaAdyacente<E>,Double> l= new HashMap<>();
-		HashMap<NodoListaAdyacente<E>, Double> s= new HashMap<>();
-		
-		for(E na:nodos.keySet()){
+		HashMap<NodoListaAdyacente<E>, Double> l = new HashMap<>();
+		HashMap<NodoListaAdyacente<E>, Double> s = new HashMap<>();
+
+		for (E na : nodos.keySet()) {
 			l.put(nodos.get(na), Double.MAX_VALUE);
 		}
-		NodoListaAdyacente<E> n1=nodos.get(nodo1);
+		NodoListaAdyacente<E> n1 = nodos.get(nodo1);
 		NodoListaAdyacente<E> n2 = nodos.get(nodo2);
 		l.put(n1, 0.0);
-		PriorityQueue<NodoListaAdyacente<E>> cola= new PriorityQueue<>();
+		PriorityQueue<NodoListaAdyacente<E>> cola = new PriorityQueue<>();
 		cola.add(n1);
-		while(s.get(n2)== null){
-			NodoListaAdyacente<E> actual= cola.poll();
-			HashMap<INodoLista<E>, Double> pesosActuales=actual.darPesos();
-			double lActual=l.get(actual);
+		while (s.get(n2) == null) {
+			NodoListaAdyacente<E> actual = cola.poll();
+			HashMap<INodoLista<E>, Double> pesosActuales = actual.darPesos();
+			double lActual = l.get(actual);
 			s.put(actual, 0.0);
 			ArrayList<INodoLista<E>> adyacentes = actual.darAdyacentes();
-			for(int i=0;i<adyacentes.size();i++){
-				NodoListaAdyacente<E> adyacenteActual=(NodoListaAdyacente<E>) adyacentes.get(i);
-				Double distanciaActualNuevo= pesosActuales.get(adyacenteActual);
-				if(distanciaActualNuevo+lActual<l.get(adyacenteActual)){
+			for (int i = 0; i < adyacentes.size(); i++) {
+				NodoListaAdyacente<E> adyacenteActual = (NodoListaAdyacente<E>) adyacentes.get(i);
+				Double distanciaActualNuevo = pesosActuales.get(adyacenteActual);
+				if (distanciaActualNuevo + lActual < l.get(adyacenteActual)) {
 					adyacenteActual.setPadre(actual);
-					adyacenteActual.modificarPesoCaminoPrevio(distanciaActualNuevo+lActual);
-					l.put(adyacenteActual, distanciaActualNuevo+lActual);
+					adyacenteActual.modificarPesoCaminoPrevio(distanciaActualNuevo + lActual);
+					l.put(adyacenteActual, distanciaActualNuevo + lActual);
 					cola.add(adyacenteActual);
 				}
 			}
-			
+
 		}
-		LinkedList<E> camino= new LinkedList<>();
-		NodoListaAdyacente<E> actual=n2;
-		while(actual!= n1){
-			E elemento= actual.getElemento();
+		LinkedList<E> camino = new LinkedList<>();
+		NodoListaAdyacente<E> actual = n2;
+		while (actual != n1) {
+			E elemento = actual.getElemento();
 			camino.addFirst(elemento);
-			actual=nodos.get(elemento);
+			actual = nodos.get(elemento);
 		}
 		return new ListaPeso<>(camino, l.get(n2));
-		
+
 	}
 
-	public NodoListaAdyacente<E> darNodo(E elem){
+	public NodoListaAdyacente<E> darNodo(E elem) {
 		return nodos.get(elem);
 	}
 
@@ -231,7 +252,5 @@ public class GrafoListaAdyacente<E> implements IGrafo<E> {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
-	
 
 }
